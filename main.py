0e7e111fe7766e7d078ee559ab8f77110770f29f
Youngseok Oh <seok7302@gmail.com>
2021-02-28 05:39:09 +0900

    File rename & 52 weeks analysis

--- main.py
+++ main.py
@@ -3,40 +3,38 @@
 # Tensorflow <= 1.14
 # finta, matplotlib, pandas, pykrx
 
-from py_krx_wr_script import *
-from datetime import datetime
+from krx_wr_script import *
+from stock_52_weeks_analysis import *
+from datetime import datetime, timedelta
 import pandas as pd
-from py_system_check import Monitor
+from system_check import Monitor
 from finta import TA
 from finta.utils import resample_calendar
-from py_plot_chart import *
+from plot_chart import *
+
+Krx_Char_folder_path = 'E:/Krx_Chart_folder'
 
 if __name__ == '__main__':
-
     From_date = '20190101'
-    stock_name = "코리아센터"
-    Today_date = datetime.today().strftime("%Y%m%d")
+    stock_name = "부광약품"
+    today_date = datetime.today().strftime("%Y%m%d")
     # Test...
-    monitor = Monitor(10) #GPU Monitor
-    monitor.stop()
-    #pykrx_scratch(From_date, Today_date) # KOSPI & KOSDAQ all stock scratch
+    # monitor = Monitor(10) #GPU Monitor
+    # monitor.stop()
+    # pykrx_scratch(From_date, today_date) # KOSPI & KOSDAQ all stock scratch
     # pykrx_daily_update()
     stock_csv = pykrx_read_csv(stock_name)
     #trainer = Model(opt)
 
     # Showing chart test...
-    print(TA.RSI(stock_csv).tail())
-    print(TA.EMA(stock_csv, 20).tail())
-    print(TA.EMA(stock_csv, 60).tail())
-    print(TA.EMA(stock_csv, 120).tail())
-    #plot_technical_indicators(stock_csv, 30)
+    # print(TA.RSI(stock_csv).tail())
+    # print(TA.EMA(stock_csv, 20).tail())
+    # # print(TA.EMA(stock_csv, 60).tail())
+    # print(TA.EMA(stock_csv, 120).tail())
+    # #plot_technical_indicators(stock_csv, 30)
 
-    ### To do list ###
-    # 1. Krx daily chart update (clear)
-    # 2. Reading train set (Clear)
-    # 3. Showing chart
-    # 4. Feature extraction
-    # 5. Technical Indicator save in csv
-    # 6. Beat or lose
-    # 7. EPS/DIV ... save in csv
-    # 8. Searching 52 weeks high - highest price
+    # 52 weeks high price test...
+    one_year_ago = datetime.now() - timedelta(days=365)
+    gap_prcentage, high_price_52w = stock_52w_gap_percentage(stock_name, one_year_ago)
+    print('Gap is {}% from {}원'.format(round(gap_prcentage,4), format(high_price_52w, ',')))
+    df_52w_csv = stock_52w_update(Krx_Char_folder_path)
---
+++ stock_52_weeks_analysis.py
@@ -0,0 +1,44 @@
+# 52 weeks analysis python
+
+from datetime import datetime, timedelta
+from krx_wr_script import *
+import os
+
+def stock_52w_gap_percentage(stock_name, one_year_ago):
+    stock_csv = pykrx_read_csv(stock_name)
+    one_year_ago = datetime.now() - timedelta(days=365)
+    stock_52w = stock_csv[stock_csv['date'] >= (one_year_ago).strftime("%Y-%m-%d")]
+    high_price_52w = max(stock_52w['high'].to_numpy())
+    close_price_day = stock_csv.iloc[len(stock_csv) - 1]['close']
+    gap_percentage = (close_price_day / high_price_52w) * 100
+    return gap_percentage, high_price_52w
+
+def stock_52w_update(Krx_Char_folder_path):
+    krx_list = os.listdir(Krx_Char_folder_path)
+    one_year_ago = datetime.now() - timedelta(days=365)
+    stock_list_52w = []
+    count = 0
+    for stock_name in krx_list:
+        stock_csv = pykrx_read_csv(stock_name)
+        close_price_day = stock_csv.iloc[len(stock_csv) - 1]['close']
+        stock_52w_csv = stock_csv[stock_csv['date'] >= (one_year_ago).strftime("%Y-%m-%d")]
+        high_price_52w = max(stock_52w_csv['high'].to_numpy())
+        if high_price_52w == 0:
+            print('This stock has issue for administration.')
+            continue
+        stock_date_52w = stock_52w_csv[stock_52w_csv['high'] == max(stock_52w_csv['high'])]['date']
+        #print(stock_date_52w)
+        if high_price_52w > close_price_day:
+            gap_percentage = round(((high_price_52w / close_price_day) * 100) - 100, 4)
+            stock_list_52w.append([str(stock_name), str(stock_date_52w.iloc[0]), float(gap_percentage), high_price_52w])
+            print('Stock : {}, 52w high date : {}, Gap : {}%, 52w high price : {}'.format(stock_name, stock_date_52w.iloc[0], gap_percentage, high_price_52w))
+            count += 1
+        elif high_price_52w < close_price_day:
+            gap_percentage = round(((close_price_day / high_price_52w) * 100) - 100, 4)
+            print('{} is already hit the 52 weeks target! Going up {}%'.format(stock_name, gap_percentage))
+    df = pd.DataFrame(stock_list_52w)
+    df = df.rename(columns={0 :'stock',1 : 'date', 2 : 'gap', 3 : 'high'})
+    df_csv = df.sort_values(by='gap', ascending=True)
+    df_csv.to_csv('results/' + '52_weeks_analysis.csv', encoding='utf-8', index=False, header=True)
+    return df_csv
+
