6e8509448d8859c513561c5e212c69b993b3801c
Kwangyeop Yoo <y900929@gmail.com>
2021-03-09 00:15:43 +0900

    모니터링 시작 알림 발송

--- main_ganda_stock.py
+++ main_ganda_stock.py
@@ -1,10 +1,12 @@
 import stockapi.stock_api as stock
 import time
 import telegramapi.telegram_api as telegram
+from datetime import datetime
 
-stock_dict1 = {'name': '마크로젠', 'target_price': 34800}
-stock_dict2 = {'name': 'SK하이닉스', 'target_price': 140000}
-target_stock_dict_list = [stock_dict1, stock_dict2]
+stock_dict1 = {'name': '강원랜드', 'target_price': 26700}
+stock_dict2 = {'name': '바텍', 'target_price': 27600}
+stock_dict3 = {'name': '마크로젠', 'target_price': 35000}
+target_stock_dict_list = [stock_dict1, stock_dict2, stock_dict3]
 
 stock.login()  # 서버 로그인
 all_stock_dict_list = stock.get_all_stock_list()  # 모든 종목명 조회
@@ -15,12 +17,22 @@ for stock_dict in target_stock_dict_list:
 
 chat_id = telegram.get_chat_id('간다! 알리미')
 idx = 0
+today_ymd = datetime.today().strftime("%Y/%m/%d")  # YYYY/mm/dd 형태의 시간 출력
+first_message = "★ " + today_ymd + " 모니터링 시작 ★\n"
+for target_stock_dict in target_stock_dict_list:
+    stock_code = target_stock_dict.get('shcode')
+    stock_name = target_stock_dict.get('name')
+    stock_current_price = stock.get_stock_current_price(shcode=stock_code)  # 현재가 조회
+    stock_target_price = target_stock_dict.get('target_price')
+    first_message += "[" + stock_name + "] " + str(stock_current_price) + " -> " + str(stock_target_price) + "\n"
+
+telegram.send_message(chat_id=chat_id, message=first_message)
 while 1:
-    for taget_stock_dict in target_stock_dict_list:
-        stock_code = taget_stock_dict.get('shcode')
-        stock_name = taget_stock_dict.get('name')
+    for target_stock_dict in target_stock_dict_list:
+        stock_code = target_stock_dict.get('shcode')
+        stock_name = target_stock_dict.get('name')
         stock_current_price = stock.get_stock_current_price(shcode=stock_code)  # 현재가 조회
-        stock_target_price = taget_stock_dict.get('target_price')
+        stock_target_price = target_stock_dict.get('target_price')
 
         if int(stock_current_price) == int(stock_target_price):
             text_message = '종목명: ' + stock_name + ' / 현재가: ' + str(stock_current_price) + ' / 매수목표가: ' + str(stock_target_price)
