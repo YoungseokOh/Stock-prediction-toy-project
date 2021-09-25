b0b3cf06256de98e500605fc6cdbd6c2e0280664
seok436 <seok7302@gmail.com>
2021-09-25 21:38:56 +0900

    Backtesting update - Buy&Hold 와 EmaCross 전략 백테스팅

--- backtesting.py
+++ backtesting.py
@@ -14,7 +14,7 @@ class EmaCross(bt.Strategy):
     # list of parameters which are configurable for the strategy
     params = dict(
         p_short=5,  # period for the fast moving average
-        p_middle=20,  # period for the slow moving average
+        p_middle=30,  # period for the slow moving average
         p_long=100,
     )
 
@@ -29,37 +29,46 @@ class EmaCross(bt.Strategy):
     def next(self):
         current_stock_price = self.data.close[0]
 
-        if not self.position:  # not in the market
-            if self.crossover < 0:  # if fast crosses slow to the upside
+        if not self.position:
+            # available_stocks = self.broker.getcash() / current_stock_price
+            # self.buy(size=int((available_stocks) * 0.99))
+            if self.crossover > 0:  # if fast crosses slow to the upside
                 available_stocks = self.broker.getcash() / current_stock_price
                 self.buy(size=int((available_stocks) * 0.3))
-            if self.rsi_14 < 20:
+            if self.rsi_14 < 30:
                 # print(self.broker.getcash())
                 available_stocks = self.broker.getcash() / current_stock_price
-                self.buy(size=int((available_stocks) * 0.7))
-        elif self.position:
-            if self.crossover < 0:
-                available_stocks = self.broker.getcash() / current_stock_price
-                self.buy(size=int((available_stocks) * 0.2))
-            if self.rsi_14 < 30:
-                available_stocks = self.broker.getcash() / current_stock_price
-                self.buy(size=int((available_stocks) * 0.15))
+                self.buy(size=int((available_stocks) * 0.99))
+        # elif self.position:
+        #     if self.crossover > 0:
+        #         available_stocks = self.broker.getcash() / current_stock_price
+        #         self.buy(size=int((available_stocks) * 0.3))
+        #     if self.rsi_14 < 40:
+        #         available_stocks = self.broker.getcash() / current_stock_price
+        #         self.buy(size=int((available_stocks)))
         # if self.rsi_14 < 10:
         #     # print(self.broker.getcash())
         #     available_stocks = self.broker.getcash() / current_stock_price
         #     self.buy(size=int((available_stocks) * 0.5))
 
-        elif self.crossover > 0:  # in the market & cross to the downside
+
+
+        # elif self.crossover < 0:  # in the market & cross to the downside
+        #     available_stocks = self.broker.getcash() / current_stock_price
+        #     if int((self.holding) * 0.15) > 0:
+        #         self.sell(size=int((self.holding) * 0.5))
+        #     else:
+        #         self.close()
+        # #     # self.close()  # close long position
+        if self.crossover < 0:  # in the market & cross to the downside
             available_stocks = self.broker.getcash() / current_stock_price
             if int((self.holding) * 0.15) > 0:
-                self.sell(size=int((self.holding) * 0.5))
-            else:
                 self.close()
-            # self.close()  # close long position
+
         if self.rsi_14 >= 70:
             available_stocks = self.broker.getcash() / current_stock_price
             if int((self.holding) * 0.15) > 0:
-                self.sell(size=int((self.holding) * 0.25))
+                self.sell(size=int((self.holding) * 0.5))
             else:
                 self.close()
 
@@ -85,15 +94,19 @@ util_bt = util()
 Finance = Finance()
 stats = []
 stock_name = '코리아센터'
-stock_list = util.read_folder_list(True, util_bt.Krx_Char_folder_path)
+# stock_list = util.read_folder_list(True, util_bt.Krx_Char_folder_path)
+count = 0
+finance_csv = Finance.load_data(Finance.bt_fin_data)
+
+# Data cleaning
+# 시가총액 2천억 이상
+finance_csv['시가총액'] = finance_csv['시가총액'].str.replace(',', '').astype(float)
+finance_csv = finance_csv[finance_csv['시가총액'] > 2000]
+# PBR 1 이하
+finance_csv = finance_csv[finance_csv['PBR'] < 1]
+stock_list = finance_csv['종목명'].tolist()
 for i in tqdm(stock_list):
     stock_csv = pykrx_read_csv(i, util_bt.Krx_Char_folder_path)
-    print(Finance.bt_fin_data)
-    finance_csv = Finance.load_data(Finance.bt_fin_data)
-
-    ## Optional
-    # refine_data
-
     if len(stock_csv) < 30:
         continue
     for w in Finance.filter_words:
@@ -123,15 +136,19 @@ for i in tqdm(stock_list):
     # print('* earning rate : %.2f %%' % ((final_value - start_value)/ start_value * 100.0))
 
     earning_rate = round(((final_value - start_value)/ start_value * 100.0),2)
-    buy_hold = (((stock_csv.iloc[len(stock_csv)-1].close - stock_csv.iloc[0].close) / stock_csv.iloc[0].close) * 100)
+    buy_hold = (((stock_csv.iloc[len(stock_csv)-1].close - stock_csv.iloc[30].close) / stock_csv.iloc[30].close) * 100)
     final_value = locale.format_string('%d', final_value, grouping=True)
 
     stats.append({'Stock' : i,
                 'earning rate': '%.2f%%' % earning_rate,
-                  'final_value': "%s won" % final_value,
+                  'final value': "%s won" % final_value,
                   'buy & hold': '% .2f%%' % buy_hold,
                   'win': earning_rate > buy_hold})
     # print(i)
-print(tabulate(stats, headers='keys'))
 
+print(tabulate(stats, headers='keys'))
+df_stats = pd.DataFrame(stats)
+print("avg. earning rate %% (buy&hold) : %.2f%%" %round(df_stats['buy & hold'].str.replace('%', '').astype(float).sum() / len(df_stats), 2))
+print("avg. earning rate %% (EmaCross) : %.2f%%" %round(df_stats['earning rate'].str.replace('%', '').astype(float).sum() / len(df_stats), 2))
+print("avg. winning rate %% : %.2f%%" % round(df_stats['win'][df_stats['win'] > 0].sum() / len(df_stats) * 100, 3))
 cerebro.plot()  # and plot it with a single command
